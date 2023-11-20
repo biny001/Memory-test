e6b9d0d8a86a0c800ff79be6c9dd6e7b3b39db32
Binyamin Tamirat <biny01amin@gmail.com>
2023-11-20 08:50:30 +0000

    additional changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ function Navbar({ score, highScore }) {
   return (
     <nav className="navbar">
       <h1>Memory Test</h1>
+      <p className="rule">Dont click the image more than once!!!</p>
       <ScoreBoard
         score={score}
         highScore={highScore}
